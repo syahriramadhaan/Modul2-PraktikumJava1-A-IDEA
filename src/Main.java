dee583053d76e8fd84cac028bb27665a961e5f80
syahriramadhann <ramadhansyahri0912@gmail.com>
2021-06-12 17:55:15 +0800

    Enkapsulasi

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
         Mahasiswa m1 = new Mahasiswa("19630192","Syahra","07-07-2007");
         m1.tampilkanAtribut();
         System.out.println(m1.getNama());
+
+        System.out.println("MAAF PAK Banyak commit yang terulang");
     }
 }
